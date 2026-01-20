0b31b56d0c313b3028b3fcb5076434a0ab79778e
EhabKhalil0802 <ehabkhalil@cmail.carleton.ca>
2026-01-19 21:37:25 -0500

    Lab 1

--- Lab1/Task5.1.py
+++ Lab1/Task5.1.py
@@ -1,19 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Given specs
-fs = 500          # sampling frequency (Hz)
-f = 10            # signal frequency (Hz)
-A = 5             # amplitude
-duration = 1.0    # seconds
+fs = 500          
+f = 10          
+A = 5             
+duration = 1.0   
 
-# Time vector: 1 second of samples at 500 Hz -> 500 samples
 t = np.arange(0, duration, 1/fs)
 
-# 10 Hz sinusoid
 x = A * np.sin(2 * np.pi * f * t)
 
-# Plot
 plt.plot(t, x)
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
--- Lab1/Task5.2.py
+++ Lab1/Task5.2.py
@@ -1,25 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Given specs
-fs = 500          # sampling frequency (Hz)
-f = 10            # signal frequency (Hz)
-A = 5             # amplitude
-duration = 1.0    # seconds
+fs = 500          
+f = 10            
+A = 5             
+duration = 1.0    
 
-# Time vector
 t = np.arange(0, duration, 1/fs)
 
-# Clean 10 Hz sinusoid
 clean_signal = A * np.sin(2 * np.pi * f * t)
 
-# Generate Gaussian noise (mean=0, var=1)
 noise = 0.25 * np.random.randn(len(t))
 
-# Noisy signal
 noisy_signal = clean_signal + noise
 
-# Plot both signals
 plt.plot(t, clean_signal, label="Clean Signal")
 plt.plot(t, noisy_signal, label="Noisy Signal", alpha=0.7)
 
@@ -28,7 +22,5 @@ plt.ylabel("Amplitude")
 plt.title("10 Hz Signal with Gaussian Noise")
 plt.legend()
 plt.grid(True)
-
-# Save the figure
 plt.savefig("Task4_2.png")
 plt.show()
